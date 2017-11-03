e86f602aaa69f20ae2ac4f4f0aa4fd960f95ed10
Loouis Low <loouis@dogsbark.net>
2017-11-03 14:12:16 +0800

    typos

--- install-node.sh
+++ install-node.sh
@@ -13,46 +13,47 @@
 #
 
 function runas_root() {
-    if [ "$(whoami &2>/dev/null)" != "root" ] && [ "$(id -un &2>/dev/null)" != "root" ]
-    then
-        echo -e "\e[38;5;198m[kraft]\e[95m Permission denied."
-    exit 1
-    fi
+  if [ "$(whoami &2>/dev/null)" != "root" ] &&
+     [ "$(id -un &2>/dev/null)" != "root" ]
+  then
+    echo -e "\e[38;5;198m[kraft]\e[95m Permission denied."
+  exit 1
+  fi
 }
 
 function show_header() {
-    echo -e "\e[95m----------------------------------------------"
-    echo -e "\e[95m Kraft, Modular UI Designing Tool"
-    echo -e "\e[95m----------------------------------------------"
-    echo -e "\e[95m 0.0.0.1"
-    echo -e "\e[95m----------------------------------------------"
+  echo -e "\e[95m----------------------------------------------"
+  echo -e "\e[95m Kraft, Modular UI Designing Tool"
+  echo -e "\e[95m----------------------------------------------"
+  echo -e "\e[95m 0.0.0.1"
+  echo -e "\e[95m----------------------------------------------"
 }
 
 function install_nodejs() {
-    echo -e "\e[38;5;198m[kraft]\e[95m installing > node.js"
-    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-    apt install -y nodejs
+  echo -e "\e[38;5;198m[kraft]\e[95m installing > node.js"
+  curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+  apt install -y nodejs
 }
 
 function node_modules() {
-    echo -e "\e[38;5;198m[kraft]\e[95m installing > node modules"
-    npm install -g http
-    npm install --unsafe-perm
+  echo -e "\e[38;5;198m[kraft]\e[95m installing > node modules"
+  npm i -g http
+  npm i --unsafe-perm
 }
 
 function rm_node_modules() {
-   echo -e "\e[38;5;198m[kraft]\e[95m cleaning > node modules"
-   find core/ -name "node_modules" -type d -exec rm -r "{}" \;
+  echo -e "\e[38;5;198m[kraft]\e[95m cleaning > node modules"
+  find core/ -name "node_modules" -type d -exec rm -r "{}" \;
 }
 
 function build_perms() {
-   echo -e "\e[38;5;198m[kraft]\e[95m changing > ownership..."
-   chown -cR loouis:loouis *
+  echo -e "\e[38;5;198m[kraft]\e[95m changing > ownership..."
+  chown -cR loouis:loouis *
 }
 
 function run_server() {
-   echo -e "\e[38;5;198m[kraft]\e[95m initialize > sakura server"
-   node server.js
+  echo -e "\e[38;5;198m[kraft]\e[95m initialize > sakura server"
+  node server.js
 }
 
 ### init
