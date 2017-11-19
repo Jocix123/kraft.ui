0bafc66c30111a9d17f4cb185906be215ae7f7f6
Loouis Low <loouis@dogsbark.net>
2017-11-19 14:58:55 +0800

    remove semicolons

--- kraft.sh
+++ kraft.sh
@@ -14,17 +14,25 @@
 
 binary=$(basename $0)
 
+# ansi
+blue='\e[94m'
+green='\e[92m'
+red='\e[91m'
+nc='\033[0m'
+normal=$(tput sgr0)
+title="${blue}[kraft]${nc}"
+
 function runas_root() {
   if [ "$(whoami &2> /dev/null)" != "root" ] &&
      [ "$(id -un &2> /dev/null)" != "root" ]
   then
-    echo "[kraft] permission denied"
+    echo "$title permission denied"
   exit 1
   fi
 }
 
 function install_nodejs() {
-  echo "[kraft] installing > node.js"
+  echo "$title installing > node.js"
   curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
   apt install -y nodejs
   sudo npm i -g n
@@ -32,24 +40,25 @@ function install_nodejs() {
 }
 
 function node_modules() {
-  echo "[kraft] cleaning > node modules"
+  echo "$title cleaning > node modules"
   find core/ -name "node_modules" -type d -exec rm -r "{}" \;
 }
 
 function run_server() {
-  echo "[kraft] initialize > app server"
+  echo "$title initialize > app server"
   node server.js
 }
 
 
-echo "[kraft] usage: $binary --help"
+echo "$title usage: $binary --help"
 
 while test "$#" -gt 0;
 do
   case "$1" in
     -h|--help)
     shift
-      echo "USAGE:"
+      echo "${bold}USAGE:${normal}"
+      echo
       echo "-h, --help          Display this message"
       echo "-i, --install       Setup application"
       echo "-c, --clean         Flush modules"
--- server.js
+++ server.js
@@ -10,40 +10,40 @@
 
 // paths
 
-var core = __dirname + '/core/';
-var views = __dirname + '/render/';
+var core = __dirname + '/core/'
+var views = __dirname + '/render/'
 
 // module dependencies
 
-const framework = require(core + 'sframework');
-const http = require('http');
+const framework = require(core + 'sframework')
+const http = require('http')
 const httpServer = http.Server(app);
-const consoleDetail = require('morgan');
-const bodyParser = require('body-parser');
-const openWebBrowser = require('opn');
+const consoleDetail = require('morgan')
+const bodyParser = require('body-parser')
+const openWebBrowser = require('opn')
 
 // environment
 
-var env = require('./env/config');
+var env = require('./env/config')
 
 // get renderer
 
-var app = framework();
+var app = framework()
 
 // get api request parameters
 
 app.use(bodyParser.urlencoded({
   extended: false
-}));
-app.use(bodyParser.json());
+}))
+app.use(bodyParser.json())
 
 console.log('[kraft] piping source...');
-app.use(framework.static(views));
+app.use(framework.static(views))
 
 app.get('/', function(req, res) {
-  console.log('[kraft] rendering script...');
-  res.sendFile(views + 'app.html');
-});
+  console.log('[kraft] rendering script...')
+  res.sendFile(views + 'app.html')
+})
 
 app.use(function(err, req, res, next){
   if(err){
